53ed08d86cb357f5ce670ec98a4648e5cc46d85f
Alexandre Doneux <he201962@students.ephec.be>
2023-05-06 22:35:14 +0200

    Moving final version to Github

---
+++ hackaton/src/components/InsideStructure.js
@@ -0,0 +1,265 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import {ThemeProvider, createTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { unstable_styleFunctionSx, styled } from '@mui/system';
+import Grid from '@mui/material/Grid';
+import Rating from '@mui/material/Rating';
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+//import { styled } from '@mui/material/styles';
+
+
+import theme from '../index.js'
+import ListOrdi from './listOrdi.js';
+import ListSmartphone from './listSmartphone.js';
+import ListTablette from './listTablette.js';
+import ListAudio from './listAudio.js';
+
+import Phone1 from './phone1.js';
+import Phone2 from './phone2.js';
+
+
+
+/*
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#E4F2AE',
+      },
+      secondary: {
+        main: '#1E943C',
+        button: '#10812D'
+      },
+      box: {
+        main: "#BED005",
+        secondary: "#D4E522"
+      }
+    },
+});
+*/
+
+
+//Les balises HTML de base ne peuvent pas être modifiée avec sx={{.....}}. On doit créer un nouveau 
+// type de balise à partir de celles-ci pouvant utiliser sx. 
+const Div = styled('div')(unstable_styleFunctionSx);
+const H1 = styled('h1')(unstable_styleFunctionSx);
+const P = styled('p')(unstable_styleFunctionSx);
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  textAlign: 'center',
+}));
+
+
+class Acceuil extends Component{
+    render(){
+      return(
+        <Div sx={{bg:"#FFFFFF"}}>
+          <Div sx={{pt:30, pb:40, height:400}}>
+            <P sx={{textAlign: 'center', fontSize:70}}>Welcome to E.T.Choice</P>
+          </Div>
+          <Div >
+            <P sx={{textAlign:'left', fontSize:40, pl:4, mx:4, bg:"theme.primary.main"}} style={{ color: 'black', backgroundColor: '#40B78F' }}>Comment se débrouillent les marques ?</P>
+            
+            <Grid container spacing={12} sx={{px:12, py:8}}>
+              <Grid item xs={4} sx={{bg:'#40B78F'}}>
+                <Item>
+                  <Div sx={{ display: 'flex', p:2 }}>
+                    <img src="fairphone.png" width="50%" height="50%" ></img>
+                    <Div>
+                      <h2>Fairphone</h2>
+                      <Div sx={{pl:2}}>
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      </Div>
+                    </Div>
+                    
+                  </Div>
+                </Item>
+
+              </Grid>
+              <Grid item xs={4}> 
+              <Item>
+                  <Div sx={{ display: 'flex', p:2 }}>
+                    <img src="apple.png" width="50%" height="50%" ></img>
+                    <Div>
+                      <h2>Apple</h2>
+                      <Div sx={{pl:2}}>
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      </Div>
+                    </Div>
+                    
+                  </Div>
+                </Item>
+              </Grid>
+              <Grid item xs={4}> 
+              <Item>
+                  <Div sx={{ display: 'flex', p:2 }}>
+                    <img src="samsung.png" width="50%" height="50%" ></img>
+                    <Div>
+                      <h2>Samsung</h2>
+                      <Div sx={{pl:2}}>
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      </Div>
+                    </Div>
+                    
+                  </Div>
+                </Item>
+              </Grid>
+              <Grid item xs={4}> 
+              <Item>
+                  <Div sx={{ display: 'flex', p:2 }}>
+                    <img src="lg.png" width="50%" height="50%" ></img>
+                    <Div>
+                      <h2>LG</h2>
+                      <Div sx={{pl:2}}>
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      </Div>
+                    </Div>
+                    
+                  </Div>
+                </Item>
+              </Grid>
+              <Grid item xs={4}> 
+              <Item>
+                  <Div sx={{ display: 'flex', p:2 }}>
+                    <img src="huawei.png" width="50%" height="50%" ></img>
+                    <Div>
+                      <h2>Huawei</h2>
+                      <Div sx={{pl:2}}>
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      </Div>
+                    </Div>
+                    
+                  </Div>
+                </Item>
+              </Grid>
+              <Grid item xs={4}> 
+              <Item>
+                  <Div sx={{ display: 'flex', p:2 }}>
+                    <img src="wiko.png" width="50%" height="50%" ></img>
+                    <Div>
+                      <h2>Wiko</h2>
+                      <Div sx={{pl:2}}>
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="4" readOnly />
+                      
+                        <Typography component="legend">Réparabilité</Typography>
+                        <Rating name="read-only" value="3" readOnly />
+                      </Div>
+                    </Div>
+                    
+                  </Div>
+                </Item>
+              </Grid>
+            </Grid>
+
+          </Div>
+        </Div>
+      )
+    }
+}
+
+
+
+
+class InsideStructure extends Component{
+    render(){
+      return(
+        <Router>
+          <Routes>
+            <Route path="/" element={<Acceuil/>}> </Route>
+            <Route path="/listOrdi" element={<ListOrdi/>}> </Route>
+            <Route path="/listSmartphone" element={<ListSmartphone/>}> </Route>
+            <Route path="/listTablette" element={<ListTablette/>}> </Route>
+            <Route path="/listAudio" element={<ListAudio/>}> </Route>
+
+            <Route path="/phone/1" element={<Phone1/>}> </Route>
+            <Route path="/phone/2" element={<Phone2/>}> </Route>
+            
+          </Routes>
+        </Router>
+      )
+    }
+    
+  }
+ // <Route path="/cours/:cours/quiz/:quiz" element={<Quiz/>}></Route>
+
+export default InsideStructure;
--- hackaton/src/index.js
+++ hackaton/src/index.js
@@ -1,17 +1,120 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+import InsideStructure from './components/InsideStructure';
+
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+
+
+import {ThemeProvider, createTheme } from '@mui/material/styles';
+import { unstable_styleFunctionSx, styled } from '@mui/system';
+
+
+
+//Les balises HTML de base ne peuvent pas être modifiée avec sx={{.....}}. On doit créer un nouveau 
+// type de balise à partir de celles-ci pouvant utiliser sx. 
+const Div = styled('div')(unstable_styleFunctionSx);
+const Img = styled('img')(unstable_styleFunctionSx);
+
+
+// On définit un theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#40B78F',
+    },
+    secondary: {
+      main: '#FFFFFF',
+      button: '#40B78F'
+    },
+    box: {
+      main: "#10812D",
+      secondary: "#E4F2AE"
+    }
+  },
+});
+
+
+
+
+
+const element = (
+  
+  <ThemeProvider theme={theme} >
+    <Box width="auto" height="auto"  >
+      <AppBar position="static" backgroundColor="primary.main"> 
+        <Box width="auto" height="auto"  sx={{ display: 'flex' }}>
+          
+          <Img width="250px"  sx={{ml:4, p:2}} src="logoETChoice.png" onClick={() => window.location = "/"}></Img>
+          
+        
+          <Div sx={{ml:"auto", mr:3, display: 'flex'}}>
+          
+          <Div sx={{ml:3, my:"auto"}}> 
+            <Button variant="contained" sx={{ml:1, mr:2, bgcolor:"secondary.button", fontSize:20, border:2}} onClick={() => window.location = "/listOrdi"}>Ordinateurs</Button>
+            <Button variant="contained" sx={{ml:1, mr:2, bgcolor:"secondary.button", fontSize:20, border:2}} onClick={() => window.location = "/listSmartphone"}>Smartphones</Button>
+            <Button variant="contained" sx={{ml:1, mr:2, bgcolor:"secondary.button", fontSize:20, border:2}} onClick={() => window.location = "/listTablette"}>Tablettes</Button>
+            <Button variant="contained" sx={{ml:1, mr:2, bgcolor:"secondary.button", fontSize:20, border:2}} onClick={() => window.location = "/listAudio"}>Audio</Button>
+            <Button variant="contained" sx={{ml:7, bgcolor:"secondary.button", fontSize:20, border:2}} onClick={() => window.location = "/cours"}>Recherche</Button>
+          </Div>
+
+          </Div>
+          
+        </Box>
+      </AppBar>
+     
+      <Box width="auto" height="auto"  sx={{ display: 'flex', bg:"secondary.main" }}>
+        
+      
+        
+      </Box>
+      <Div id="main" sx={{bgcolor:"#secondary.main", height:1}}>
+        
+        
+        
+      </Div>
+      
+      
+    </Box>
+  </ThemeProvider>
+  
+  
+
 );
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <InsideStructure/>,
+  document.getElementById('main')
+);
+
+
+
+/*
+<AppBar position="absolute" sx={{mt:"2", p:2, top: 'auto', bottom: 0, bgcolor:"secondary.main"}}> 
+        <Div sx={{textAlign:"center"}}>
+          Nous contacter: Responsable admin@admin.be
+          <br/><br/>
+          @copyright
+        </Div>
+      </AppBar>
+      */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+export default theme;
